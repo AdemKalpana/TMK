5d5bb0fe1d0d323f8b2150fd6892096346e51d80
kalpana Adem <kalpana.abap35@gmail.com>
2021-11-10 18:40:27 +0000

    Second Commit

--- webapp/view/Main.view.js
+++ webapp/view/Main.view.js
@@ -1,34 +1,55 @@
-sap.ui.jsview("mkt56.view.Main",{
+sap.ui.jsview("mkt56.view.Main", {
 
-    getControllerName: function(){
-        return "mkt56.controller.Main";
-    },
+	getControllerName: function() {
+		return "mkt56.controller.Main";
+	},
+	createContent: function(oController) {
+		//oInp is a UI5 object not an HTML Object
+		//@Runtime UI5 Renderer will produce the HTML
+		var form = new sap.ui.layout.form.SimpleForm({
+			title: "User Details",
+			content: [
 
-    //always every JS view has this standard method
-    //which is entitled to receive controller object
-    createContent: function(oAnubhav){
 
-        var oInput = new sap.m.Input("idInp",{
-            value: "SAP UI5 Learnings"
-        });
+				new sap.m.Label({
 
-        var oBtn = new sap.m.Button({
-                text: "welcome",
-                //press is an event which calls event handler
-                //the processing logic is in my controller class
-                //explicitly passsing the CONTROLLER object to EVENT HANDLER
-                //SO this pointer CAN BE INITIALIZED to Controller object
-                press: [oAnubhav.clickMe, oAnubhav]
-        });
+					text: "User Name:"
 
-         var oBtn2 = new sap.m.Button("idBtn2",{
-                text: "I am inactive"
-                //press is an event which calls event handler
-                //the processing logic is in my controller class
-                //press: oAnubhav.clickMe
-        });
+				}),
 
-        return [oInput, oBtn, oBtn2];
-    }
+				new sap.m.Input("idUser", {
+
+					type: sap.m.InputType.Text,
+
+					placeholder: "Enter a Name..."
+
+				}),
+
+				new sap.m.Label({
+
+					text: "Password:"
+
+				}),
+
+				new sap.m.Input("idPassword", {
+
+					type: sap.m.InputType.Password,
+
+					placeholder: "Password"
+
+				}),
+				new sap.m.Button("idBtn", {
+					text: "Click me",
+					icon: "sap-icon://program-triangles",
+					press: [oController.onLogin, oController]
+				})
+
+			]
+
+		});
+
+		return [form];
+
+	}
 
 });
